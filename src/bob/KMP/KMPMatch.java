581f7812c513cea6e91a929c2f8141cc1e5c06b7
bob <329733876@qq.com>
2021-02-19 19:25:26 +0800

    kmp

--- src/bob/KMP/KMPMatch.java
+++ src/bob/KMP/KMPMatch.java
@@ -1,7 +1,25 @@
 package bob.KMP;
 
 public class KMPMatch {
+
+/* 
+KMP算法核心，比较前缀和后缀子序列，取最大公式元素长度，得到部分匹配数组PMT
+将PMT数组右移一位，第一位补-1，获得next数组 ，
+移动下一位长度是 j-next[j] (j是模式串的位置)
+*/
+
+
     public static void main(String[] args) {
 
     }
+
+
+    public int partialMatchTable(){
+        return 0 ;
+    }
+
+
+
+
+
 }
