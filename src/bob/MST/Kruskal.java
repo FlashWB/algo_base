950ba6722703ec37e03da0c3aba6b3b01aeafc85
FlashWB <329733876@qq.com>
2021-03-23 19:55:11 +0800

    并查集+ kruskal

--- src/bob/MST/Kruskal.java
+++ src/bob/MST/Kruskal.java
@@ -1,5 +1,7 @@
 package bob.MST;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashSet;
 
 /* 
@@ -17,40 +19,147 @@ kruskal算法和prim相似，都是求最小生成树
 注意：并查集不能将在同一组的元素拆分为两组。
 
 
-如何不构成回路：加入的边的两个顶点不能都指向同一 个终点，否则将构成回路
+如何不构成回路：加入的边的两个顶点不能都指向同一 个终点(即没有相同祖宗)，否则将构成回路
 */
 public class Kruskal {
-    static int n; // 结点数
-    static int m; // 路径数
-    static int check[];
-    static int count = 0;
-    public static HashSet<node> set = new HashSet<>();// 存储已经访问过的路径信息
-    public static node[] list = new node[515556];// 存储路径信息，起点，终点，路径长
+    private static int n; // 结点数
+    private static int m; // 路径数
+    private static int parent[]; // 父节点集合，判断是否同一个根节点，是否同一个祖宗
+    private char[] vertexs; // 顶点数组
+    private int[][] matrix; // 邻接矩阵
+    private int count = 0;
+
+    public static final int INF = Integer.MAX_VALUE;
+    public static HashSet<Edata> set = new HashSet<>();// 存储已经访问过的路径信息
 
     public static void main(String[] args) {
+        char[] vertexs = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+        int matrix[][] = { /* A *//* B *//* C *//* D *//* E *//* F *//* G */
+                /* A */ { 0, 12, INF, INF, INF, 16, 14 }, /* B */ { 12, 0, 10, INF, INF, 7, INF },
+                /* C */ { INF, 10, 0, 3, 5, 6, INF }, /* D */ { INF, INF, 3, 0, 4, INF, INF },
+                /* E */ { INF, INF, 5, 4, 0, 2, 8 }, /* F */{ 16, 7, 6, INF, 2, 0, 9 },
+                /* G */{ 12, INF, INF, INF, 8, 9, 0 } };
+        Kruskal kruskal = new Kruskal(vertexs, matrix);
+        kruskal.print();
+        Edata[] edges = kruskal.gEdatas();
+        System.out.print("排序前：");
+        System.out.println(Arrays.toString(edges));
+        kruskal.sortEdges(edges);
+        System.out.print("排序后：");
+        System.out.println(Arrays.toString(edges));
+
+        // 并查集初始化
+        parent = new int[edges.length+1];
+        for (int i = 1; i < edges.length + 1; i++) {
+            parent[i] = i;
+        }
+        for (Edata eitem : edges) {
+            if(find(Arrays.binarySearch(vertexs, eitem.start))!=find(Arrays.binarySearch(vertexs, eitem.end))){
+            merge(Arrays.binarySearch(vertexs, eitem.start),Arrays.binarySearch(vertexs, eitem.end) );
+            set.add(eitem);
+            }
+        }
+        // for (Edata eitem : edges) {
+        //     // 如果起点和终点不能在同一棵树上，否则会闭环
+        //     if(find(Arrays.binarySearch(vertexs, eitem.start))==find(Arrays.binarySearch(vertexs, eitem.start))){
+        //         set.add(eitem);
+        //     }
+        // }
+        System.out.println("最小生成树：");
+        for(Edata e : set){
+            System.out.print("<"+e.start+","+e.end+">="+e.length);
+            // System.out.printf("<%c,%c>=%d",e.start,e.end,e.length);
+        }
 
     }
 
-    class node implements Comparable<node> {
-        int start;
-        int end;
+    // 构造 器
+    public Kruskal(char[] ver, int[][] mat) {
+        n = ver.length;
+        m = 0;
+        matrix = new int[n][n];
+        vertexs = new char[n];
+
+        // 初始化顶点
+        vertexs = Arrays.copyOf(ver, n);
+        // 初始化边
+        for (int i = 0; i < n; i++) {
+            matrix[i] = Arrays.copyOf(mat[i], n);
+        }
+        // 统计边的个数
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (matrix[i][j] != INF) {
+                    m++;
+                }
+            }
+        }
+    }
+
+    // 打印
+    public void print() {
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+
+    public Edata[] gEdatas() {
+        int index = 0;
+        Edata[] edatas = new Edata[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (matrix[i][j] != INF) {
+                    Edata edata = new Edata(vertexs[i], vertexs[j], matrix[i][j]);
+                    edatas[index++] = edata;
+                }
+            }
+        }
+        return edatas;
+    }
+
+    // 冒泡排序，每轮交换比较n-1-i次
+    public void sortEdges(Edata[] edges) {
+        for (int i = 0; i < edges.length - 1; i++) {
+            for (int j = 0; j < edges.length - i - 1; j++) {
+                if (edges[j].compareTo(edges[j + 1]) > 0) {
+                    Edata tmp = edges[j + 1];
+                    edges[j + 1] = edges[j];
+                    edges[j] = tmp;
+                }
+            }
+        }
+    }
+
+    // 边对象
+    class Edata implements Comparable<Edata> {
+        char start;
+        char end;
         int length;
 
-        public node(int start, int end, int length) {
-            this.end = start;
+        public Edata(char start, char end, int length) {
+            this.start = start;
             this.end = end;
             this.length = length;
         }
 
         @Override
-        public int compareTo(node o) {
+        public int compareTo(Edata o) {
             // TODO Auto-generated method stub
             return this.length - o.length;
+        }
+
+        public String toString() {
+            return "<" + start + "," + end + ">=" + length;
         }
     }
 
     // 并查集中的并
     public static int find(int x) {
-        return check[x] == x ? x : (check[x] = find(check[x]));
+        return parent[x] == x ? x : (parent[x] = find(parent[x]));
     }
+
+    public static void merge(int x, int y) {
+        parent[find(x)] = find(y);
+    }
+
 }
--- src/bob/MST/UnionFindSets.java
+++ src/bob/MST/UnionFindSets.java
@@ -1,5 +1,8 @@
 package bob.MST;
 
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
 /*
 https://www.jianshu.com/p/89cea54d3f22
 https://zhuanlan.zhihu.com/p/93647900/
@@ -9,12 +12,17 @@ https://zhuanlan.zhihu.com/p/93647900/
 
 并查集也是使用树形结构实现的，不过不是二叉树。
 
+
+
 题目背景
-若某个家族人员过于庞大，要判断两个是否是亲戚，确实还很不容易，现在给出某个亲戚关系图，求任意给出的两个人是否具有亲戚关系。
+若某个家族人员过于庞大，要判断两个是否是亲戚，确实还很不容易，现在给出某个亲戚关系图，
+求任意给出的两个人是否具有亲戚关系。
 题目描述
-规定：x和y是亲戚，y和z是亲戚，那么x和z也是亲戚。如果x,y是亲戚，那么x的亲戚都是y的亲戚，y的亲戚也都是x的亲戚。
+规定：x和y是亲戚，y和z是亲戚，那么x和z也是亲戚。如果x,y是亲戚，那么x的亲戚都是y的亲戚，
+y的亲戚也都是x的亲戚。
 输入格式
-第一行：三个整数n,m,p，（n<=5000,m<=5000,p<=5000），分别表示有n个人，m个亲戚关系，询问p对亲戚关系。
+第一行：三个整数n,m,p，（n<=5000,m<=5000,p<=5000），分别表示有n个人，m个亲戚关系，
+询问p对亲戚关系。
 以下m行：每行两个数Mi，Mj，1<=Mi，Mj<=N，表示Mi和Mj具有亲戚关系。
 接下来p行：每行两个数Pi，Pj，询问Pi和Pj是否具有亲戚关系。
 输出格式
@@ -22,28 +30,80 @@ P行，每行一个’Yes’或’No’。表示第i个询问的答案为“具�
 
  */
 public class UnionFindSets {
-    int MAXN = 10;
     int[] par; // 假如有编号为1, 2, 3, ..., n的n个元素，用数组par[]存储元素的父节点，每个元素有且只有一个父节点
 
     public static void main(String[] args) {
-        System.out.println();
+        // 家族题解
+        int n, m, p, x, y;
+        Scanner s = new Scanner(new BufferedInputStream(System.in));
+        n = s.nextInt();
+        m = s.nextInt();
+        p = s.nextInt();
+        UnionFindSets unionFindSets = new UnionFindSets(n);
+        for (int i = 0; i < m; i++) {
+            x = s.nextInt();
+            y = s.nextInt();
+            unionFindSets.merge(x, y);
+        }
+        for (int i = 0; i < p; ++i) {
+            x = s.nextInt();
+            y = s.nextInt();
+            System.out.println(unionFindSets.find(x) == unionFindSets.find(y) ? "yes" : "no");
+        }
+
     }
 
     public UnionFindSets(int n) {
-        par = new int[n];
-        for (int i = 1; i <= n; ++i) {
+        par = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             par[i] = i;
         }
     }
 
-    // 查找树的根,一层一层访问父节点，直至根节点
+    // 查找树的根：一层一层访问父节点，直至根节点(找出祖宗)
     public int find(int x) {
-        // 压缩路径 par[x] = find(par[x])
-        return  par[x] == x  ? x : (par[x] = find(par[x]));
+        // 压缩路径 par[x] = find(par[x]) 查询过程中，把沿途的每个节点的父节点都设为根节点
+        return par[x] == x ? x : (par[x] = find(par[x]));
     }
 
-    public void merge(int i, int j){
+    public void merge(int i, int j) {
+        // 先找到两个集合的代表元素，然后将前者的父节点设为后者
         par[find(i)] = find(j);
+    }
+}
+
+/**
+ * 进一下优化，按秩压缩
+ */
+class UnionFindSets2 {
+    int[] par; // 存储父节点
+    int[] rank; // 存放秩
+
+    public UnionFindSets2(int n) {
+        this.par = new int[n + 1];
+        this.rank = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            par[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        return par[x] == x ? x : (par[x] = find(par[x]));
+    }
+
+    public void merge(int i, int j) {
+        int x = find(i);
+        int y = find(j);
+        if (rank[x] <= rank[y]) {
+            par[x] = y;
+        } else {
+            par[y] = x;
+        }
+        if (rank[x] == rank[y] && x != y) {
+            // 秩相同时，被合上根节点的秩+1
+            rank[y]++;
+        }
     }
 
 }
